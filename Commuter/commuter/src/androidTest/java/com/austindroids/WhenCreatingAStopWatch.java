5a89e8fc5876fc01fa1a55469b4f5bc12f4ab2e5
Brett Carroll <brett.carroll@gmail.com>
2014-07-27 17:08:56 -0500

    Added tests for StopWatch added Route Model.

---
+++ Commuter/commuter/src/androidTest/java/com/austindroids/WhenCreatingAStopWatch.java
@@ -0,0 +1,7 @@
+package com.austindroids;
+
+/**
+ * Created by Brett on 7/27/2014.
+ */
+public class WhenCreatingAStopWatch {
+}
---
+++ Commuter/commuter/src/main/java/com/austindroids/commuter/model/Route.java
@@ -0,0 +1,7 @@
+package com.austindroids.commuter.model;
+
+/**
+ * Created by Brett on 7/27/2014.
+ */
+public class Route {
+}
